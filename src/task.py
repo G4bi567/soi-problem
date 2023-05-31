5e833bda9f6e14a4252b6198bbbf565f0194bb99
Gabriel Teixeira Dias <g3briel58@gmail.com>
2023-05-31 12:48:23 +0000

    sauvegarde automatique

--- src/task.py
+++ src/task.py
@@ -16,7 +16,7 @@ def main():
     '''
     n = int(input())
     values =[input().strip().split() for _ in range(n)]
-    values_list=[range(values[i][0],values[i][1]+1) for i in range(n)]
+    values_list=[list(range(int(values[i][0]),int(values[i][1])+1)) for i in range(n)]
     print(values)
     
 
