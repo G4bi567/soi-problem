e46ca5881d551a37e5c808f3cfbd743573dbbf6f
Gabriel Teixeira Dias <g3briel58@gmail.com>
2023-05-31 09:54:00 +0000

    sauvegarde automatique

--- src/task.py
+++ src/task.py
@@ -15,10 +15,9 @@ def main():
     This is where the task is solved
     '''
     infos=input().strip().split()
-    nlivres=infos[0]+1
-    njours=infos[1]+1
-    for _ in range(njours):
-        
+    nlivres=int(infos[0])+1
+    njours=int(infos[1])+1
+    infos_livres=[input().strip().split() for _ in range(njours)]
 
 
 if __name__ == '__main__':
