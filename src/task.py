3f74f2d4696ce6280460baaf3e613de868b528c1
Gabriel Teixeira Dias <g3briel58@gmail.com>
2023-05-31 09:49:57 +0000

    sauvegarde automatique

--- src/task.py
+++ src/task.py
@@ -14,24 +14,7 @@ def main():
     '''
     This is where the task is solved
     '''
-    donnees=input()
-    d=donnees.strip()
-    nblivres= int(d[0])
-    bd= [0]*(nblivres+1)
-    nbjours=int(d[2])
-    d=d[4:]
-    nbre=0
-    for i in range(nbjours):
-        nbclients=int(d[nbre])
-        for n in range(nbclients):
-            if bd[int(d[nbre+2+4*n])] <=0:
-                print(1)
-                bd[int(d[nbre+2+4*n])]=0
-                bd[int(d[nbre+2+4*n])]=int(d[nbre+4+4*n])
-            else:
-                print(0)
-        nbre += 2+4*nbclients
-        bd = [x-1 for x in bd]
+    nlivres,njours=input().split()
 
 
 if __name__ == '__main__':
